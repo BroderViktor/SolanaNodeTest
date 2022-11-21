9a17b641e13f0340c2d926bc4f53dd217b850ce5
BroderViktor <viktor.asheim@gmail.com>
2022-11-21 18:35:23 +0100

    wop

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-sol = require("solana");
+const { getMint } = require("@solana/spl-token");
+
+sol = require("@solana/web3.js");
+solToken = require("@solana/spl-token");
 bs58 = require("bs58");
 
-// connection
-const connection = new sol.Connection("https://api.devnet.solana.com");
-
-// 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
-const feePayer = sol.Keypair.fromSecretKey(
-  bs58.decode("588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2")
-);
+const connection = new sol.Connection(sol.clusterApiUrl("devnet"));
+const publicKeySol = new sol.PublicKey('EsTCkUkyS4QrXELy62p1Ln39anxWGXbmjaXMdeCBu7Qm');
+const publicKeyBac = new sol.PublicKey('FFDyss8ZNKDoptctFbem76j5idS5yqWVc3kLsNhgFSpV');
 
 (async () => {
-  console.log(`Server listening on`);
-  let balance = await connection.getBalance(feePayer.publicKey);
+  let balance = await connection.getBalance(publicKeySol);
   console.log(`${balance / sol.LAMPORTS_PER_SOL} SOL`);
+
+  let BacBalance = await connection.getTokenAccountBalance(publicKeyBac);
+  console.log(BacBalance)
 })();
 
+// (async () => {
 
+//   const connection = new sol.Connection("https://api.devnet.solana.com");
+
+//   const tokenAccounts = await connection.getTokenAccountsByOwner(
+//     new sol.PublicKey('EsTCkUkyS4QrXELy62p1Ln39anxWGXbmjaXMdeCBu7Qm'),
+//     {
+//       programId: solToken.TOKEN_PROGRAM_ID,
+//     }
+//   );
+
+//   console.log("Token                                         Balance");
+//   console.log("------------------------------------------------------------");
+//   tokenAccounts.value.forEach((tokenAccount) => {
+//     const accountData = AccountLayout.decode(tokenAccount.account.data);
+//     console.log(`${new sol.PublicKey(accountData.mint)}   ${accountData.amount}`);
+//   })
+
+// })();
